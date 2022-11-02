df872d487e253e3fa4fffdbf047ab91288485f04
Aysha Hussain <ayshafajar3@gmail.com>
2022-11-02 17:02:35 +0000

    final changes to issue-1-create-an-entry

--- js/main.js
+++ js/main.js
@@ -1,18 +1,13 @@
 /* global data */
 
 var $form = document.querySelector('form');
-document.getElementById('title');
-// console.log('container', document.querySelector('.container'));
 
 var $url = document.querySelector('#Photo-URL');
-
-document.querySelector('main');
 
 var $image = document.querySelector('.image');
 
 function handleInput(event) {
   $image.src = $url.value;
-  Image.src();
 }
 
 $url.addEventListener('input', handleInput);
