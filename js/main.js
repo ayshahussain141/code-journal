56de59090793f072242da052ad3f30db2de3c421
Aysha Hussain <ayshafajar3@gmail.com>
2022-11-02 16:46:48 +0000

    final completion issue-1-create-an-entry

--- js/main.js
+++ js/main.js
@@ -1,16 +1,24 @@
 /* global data */
 
 var $form = document.querySelector('form');
+document.getElementById('title');
+// console.log('container', document.querySelector('.container'));
+
 var $url = document.querySelector('#Photo-URL');
+
+document.querySelector('main');
+
 var $image = document.querySelector('.image');
 
 function handleInput(event) {
   $image.src = $url.value;
+  Image.src();
 }
+
 $url.addEventListener('input', handleInput);
 
 $form.addEventListener('submit', function (event) {
-  event.preventDefault();
+
   var entries = {};
   entries.title = $form.elements.title.value;
   entries.url = $form.elements.url.value;
@@ -18,4 +26,6 @@ $form.addEventListener('submit', function (event) {
   entries.nextid = data.nextEntryId++;
   data.entries.push(entries);
   $form.reset();
+  $image.src = 'images/placeholder-image-square.jpg';
+  event.preventDefault();
 });
