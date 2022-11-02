b27e728a9c8492bc12d585114a34d75185eec930
Aysha Hussain <ayshafajar3@gmail.com>
2022-11-02 23:33:13 +0000

    added images issue-2

--- js/main.js
+++ js/main.js
@@ -24,3 +24,46 @@ $form.addEventListener('submit', function (event) {
   $image.src = 'images/placeholder-image-square.jpg';
   event.preventDefault();
 });
+
+function renderList(entry) {
+  var $ul = document.querySelector('ul');
+  var list = document.createElement('li');
+  list.textContent = entry.notes;
+  $ul.prepend(list);
+
+  var heading = document.createElement('h3');
+  heading.textContent = entry.title;
+  list.prepend(heading);
+
+  var image = document.createElement('img');
+  image.setAttribute('src', entry.url);
+  image.setAttribute('class', 'imagesize');
+  list.prepend(image);
+  console.log(entry);
+  console.log($ul);
+}
+
+var $ul = document.querySelector('ul');
+console.log($ul);
+
+document.addEventListener('DOMContentLoaded', function (event) {
+  for (var i = 0; i < data.entries.length; i++) {
+    renderList(data.entries[i]);
+  }
+
+});
+
+var $button = document.querySelector('.new-button');
+console.log('brotha', $button);
+
+var hidden = document.querySelector('.hidden');
+console.log(hidden);
+
+var view = document.querySelector('.visible');
+console.log(view);
+
+// var target = event.target.getAttribute('data-view');
+$button.addEventListener('click', function (event) {
+  hidden.className = view;
+  view.className = hidden;
+});
