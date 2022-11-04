74a6a564512f128d0f2eace956508619745b6495
Aysha Hussain <ayshafajar3@gmail.com>
2022-11-04 15:27:26 +0000

    completed issue-2-view-an-entry

--- js/main.js
+++ js/main.js
@@ -2,68 +2,83 @@
 
 var $form = document.querySelector('form');
 
-var $url = document.querySelector('#Photo-URL');
+var $url = document.querySelector('#photo-url');
 
 var $image = document.querySelector('.image');
 
 function handleInput(event) {
-  $image.src = $url.value;
+  $image.setAttribute('src', $url.value);
 }
 
 $url.addEventListener('input', handleInput);
 
 $form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  var object = {};
 
-  var entries = {};
-  entries.title = $form.elements.title.value;
-  entries.url = $form.elements.url.value;
-  entries.notes = $form.elements.notes.value;
-  entries.nextid = data.nextEntryId++;
-  data.entries.unshift(entries);
+  object.title = $form.elements.title.value;
+  object.url = $form.elements.url.value;
+  object.notes = $form.elements.notes.value;
+  object.nextid = data.nextEntryId;
+  data.nextEntryId++;
+  data.entries.unshift(object);
+
+  renderList(data.entries[0]);
   $form.reset();
   $image.src = 'images/placeholder-image-square.jpg';
-  event.preventDefault();
-});
 
+});
+var $ul = document.querySelector('ul');
 function renderList(entry) {
-  var $ul = document.querySelector('ul');
+
   var list = document.createElement('li');
-  list.textContent = entry.notes;
   $ul.prepend(list);
+
+  var par = document.createElement('p');
+  par.textContent = entry.notes;
 
   var heading = document.createElement('h3');
   heading.textContent = entry.title;
-  list.prepend(heading);
+
+  var newElement = document.createElement('div');
+  newElement.setAttribute('class', 'column-half');
 
   var image = document.createElement('img');
   image.setAttribute('src', entry.url);
   image.setAttribute('class', 'imagesize');
   list.prepend(image);
-  console.log(entry);
-  console.log($ul);
+
+  list.appendChild(newElement);
+  newElement.appendChild(heading);
+  newElement.appendChild(par);
 }
 
-var $ul = document.querySelector('ul');
-console.log($ul);
-
-document.addEventListener('DOMContentLoaded', function (event) {
+window.addEventListener('DOMContentLoaded', function (event) {
   for (var i = 0; i < data.entries.length; i++) {
-    renderList(data.entries[i]);
+    (renderList(data.entries[i]));
   }
 
 });
-
 var $button = document.querySelector('.new-button');
-console.log('brotha', $button);
-
 var hidden = document.querySelector('.hidden');
-console.log(hidden);
-
 var view = document.querySelector('.visible');
-console.log(view);
 
-// var target = event.target.getAttribute('data-view');
 $button.addEventListener('click', function (event) {
   hidden.className = view;
-  view.className = hidden;
+  view.className = 'hidden';
+});
+
+var anchor = document.querySelector('a');
+
+anchor.addEventListener('click', function (event) {
+  hidden.className = 'hidden';
+  view.className = view;
+
+});
+
+var saveButton = document.querySelector('.button-save');
+
+saveButton.addEventListener('click', function (event) {
+  hidden.className = 'hidden';
+  view.className = view;
 });
